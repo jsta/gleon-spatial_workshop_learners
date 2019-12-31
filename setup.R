d45a7885bacd5ad74faa153e6061eab22bd4dff3
jsta <stachel2@msu.edu>
2019-12-31 16:55:03 +0000

    fillout setup file

--- setup.R
+++ setup.R
@@ -1,1 +1,15 @@
 # This is a setup file
+
+# Install system dependencies
+# https://github.com/datacarpentry/r-raster-vector-geospatial/blob/master/.travis.yml
+
+# Install R packages
+# https://datacarpentry.org/geospatial-workshop/setup.html
+install.packages(c("dplyr", "ggplot2", "raster", "rgdal", "rasterVis", "sf"))
+# optional
+install.packages(c("mapview"))
+
+download.file(
+  "https://erddap.bco-dmo.org/erddap/files/bcodmo_dataset_680091/680091_v1_Particulate_and_dissolved_Lake_Superior_geochemistry.tsv", 
+  "data/bcodmo_superior_geomchem.tsv")
+
