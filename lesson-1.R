a5abf139ce5c41c1580ab864984b937145c2de3c
jsta <stachel2@msu.edu>
2020-01-29 15:43:15 -0500

    clear out old example

--- lesson-1.R
+++ lesson-1.R
@@ -2,9 +2,3 @@
 
 library(sf)
 library(mapview)
-
-dt <- read.table("data/bcodmo_superior_geomchem.tsv", header = TRUE)
-dt <- st_as_sf(dt, coords = c("lon", "lat"), crs = 4326)
-
-plot(dt$geometry)
-mapview(dt)
--- setup.R
+++ setup.R
@@ -1,13 +1,8 @@
-# This is a setup file
+# Download data from:
+# https://github.com/jsta/glatos-spatial_workshop_learners/releases
 
 # Install R packages
 # https://datacarpentry.org/geospatial-workshop/setup.html
 install.packages(c("dplyr", "ggplot2", "raster", "rgdal", "rasterVis", "sf", "mapview"))
 # If you encounter errors you may need to install external prerequsite programs:
-# https://github.com/datacarpentry/r-raster-vector-geospatial/blob/master/.travis.yml
-
-# Download data
-download.file(
-  "https://erddap.bco-dmo.org/erddap/files/bcodmo_dataset_680091/680091_v1_Particulate_and_dissolved_Lake_Superior_geochemistry.tsv", 
-  "data/bcodmo_superior_geomchem.tsv")
-
+# https://datacarpentry.org/geospatial-workshop/setup.html
